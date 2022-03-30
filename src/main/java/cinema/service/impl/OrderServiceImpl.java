9700a8049e6ea9ff595dd72b2010589d42bfb483
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-30 11:44:25 +0300

    created cinema app using hibernate

---
+++ src/main/java/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,38 @@
+package cinema.service.impl;
+
+import cinema.dao.OrderDao;
+import cinema.lib.Inject;
+import cinema.lib.Service;
+import cinema.model.Order;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.service.OrderService;
+import cinema.service.ShoppingCartService;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        if (shoppingCart.getTickets().isEmpty()) {
+            throw new RuntimeException("There is no tickets in shopping cart " + shoppingCart);
+        }
+        Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(shoppingCart.getTickets());
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getByUser(user);
+    }
+}
